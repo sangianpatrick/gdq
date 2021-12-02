51c60875749a772ef228fb0023b0049599138288
Patrick Maurits Sangian <patricksangian@gmail.com>
2021-12-03 03:32:14 +0800

    chore: add comment to message props

--- message.go
+++ message.go
@@ -2,8 +2,12 @@ package gdq
 
 // Message is an object that can be sent and received.
 type Message struct {
-	Key     string              `json:"key"`
+	// Key is a key of a message. Make sure it is globally unique to avoid deleting queue with the same message instance.
+	Key string `json:"key"`
+	// Headers will describe the information of a message. Make sure to fill it.
 	Headers map[string][]string `json:"headers"`
-	Value   []byte              `json:"value"`
-	Score   float64             `json:"score"`
+	// Value is a value of a message.
+	Value []byte `json:"value"`
+	// Score is a score of message that used for sorting the message.
+	Score float64 `json:"score"`
 }
